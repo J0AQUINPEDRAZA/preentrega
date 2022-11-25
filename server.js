663f99ec223d474373f1103e5f14c27cb68ec949
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2022-11-24 21:44:41 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import productsRouter from "./routes/productRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/api/productos", productsRouter);
+app.use("/api/carrito", cartRouter);
+
+const server = app.listen(PORT, () => {
+  console.log(`The server is running in: http://localhost:${PORT} 🔐`);
+});
+server.on("error", (error) => console.log(`Error en el servidor: ${error}`));
+
+export default app;
